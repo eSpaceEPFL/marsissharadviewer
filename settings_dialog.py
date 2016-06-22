da40e8978ebb84aee9e7ea90c5cfa7da98623daa
Federico <federico@fc7>
2016-06-22 16:26:01 +0200

    Added prefs retrieval

--- settings_dialog.py
+++ settings_dialog.py
@@ -20,9 +20,53 @@ class SettingsDialog(QtGui.QWidget, FORM_CLASS):
         super(SettingsDialog, self).__init__(parent)
         self.prefs = prefs
         self.setupUi(self)
+        self.set_dialog()
         self.show()
 
         print dir(self.prefs)
+
+    def set_dialog(self):
+        #MARSIS
+        self.lineMarsisDataDisk.setText(self.prefs.DISK_L2_DIR['MARSIS'])
+        self.lineMarsisSimDisk.setText(self.prefs.DISK_SIM_DIR['MARSIS'])
+
+        self.lineMarsisDataHttp.setText(self.prefs.HTTP_L2_DIR['MARSIS'])
+        self.lineMarsisSimHttp.setText(self.prefs.HTTP_SIM_DIR['MARSIS'])
+
+        if self.prefs.DATA_SOURCE['MARSIS'] == 'DISK':
+            self.radioMarsisDataDisk.setChecked(True)
+
+        if self.prefs.DATA_SOURCE['MARSIS'] == 'HTTP':
+            self.radioMarsisDataHttp.setChecked(True)
+
+        if self.prefs.SIM_SOURCE['MARSIS'] == 'DISK':
+            self.radioMarsisSimDisk.setChecked(True)
+
+        if self.prefs.SIM_SOURCE['MARSIS'] == 'HTTP':
+            self.radioMarsisSimHttp.setChecked(True)
+
+
+        #SHARAD
+        self.lineSharadDataDisk.setText(self.prefs.DISK_L2_DIR['SHARAD'])
+        self.lineSharadSimDisk.setText(self.prefs.DISK_SIM_DIR['SHARAD'])
+
+        self.lineSharadDataHttp.setText(self.prefs.HTTP_L2_DIR['SHARAD'])
+        self.lineSharadSimHttp.setText(self.prefs.HTTP_SIM_DIR['SHARAD'])
+
+        if self.prefs.DATA_SOURCE['SHARAD'] == 'DISK':
+            self.radioSharadDataDisk.setChecked(True)
+
+        if self.prefs.DATA_SOURCE['SHARAD'] == 'HTTP':
+            self.radioSharadDataHttp.setChecked(True)
+
+        if self.prefs.SIM_SOURCE['SHARAD'] == 'DISK':
+            self.radioSharadSimDisk.setChecked(True)
+
+        if self.prefs.SIM_SOURCE['SHARAD'] == 'HTTP':
+            self.radioSharadSimHttp.setChecked(True)
+
+        #MISC
+        self.lineCacheDir.setText(self.prefs.CHACHE_BASE_DIR)
 
     def accept(self):
         pass
